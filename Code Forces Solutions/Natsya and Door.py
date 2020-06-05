7647b0adfb1af8f27d914ba673574fd4c4ff6f04
ch-its <b19077@students.iitmandi.ac.in>
2020-06-05 13:17:11 +0530

    rounf 647

--- Code Forces Solutions/Natsya and Door.py
+++ Code Forces Solutions/Natsya and Door.py
@@ -1,26 +1,26 @@
+def pow2(a):
+    while a%2==0:
+        a=a//2
+    return a
+
 t=int(input())
 for i in range(t):
-    n,k=list(map(int,input().split()))
-    
-    arr=list(map(int,input().split()))
-    d=[0 for i in range(n)]
-    
-    
-    for i in range(1,n-1):
-        if arr[i]>arr[i-1] and arr[i]>arr[i+1]:
-            d[i]=1
-    pref=[0 for i in range(n)]
-    for i in range(1,n):
-        pref[i]=pref[i-1]+d[i]
+    a,b=list(map(int,input().split()))
     ans=0
-    index=0
-    anst=0
-    for l in range(n-k+1):
-        if pref[l+k-2]-pref[l]>ans:
-            ans=pref[l+k-2]-pref[l]
-            index=l
+    if (pow2(a)!=pow2(b)):
+        print(-1)
+    else:
+        c=max(a,b)
+        d=min(a,b)
+        c=c//d
+        while c>=8:
+            c=c//8
+            ans+=1
+        if c>1:
+            ans+=1
+        print(ans)
         
-        
-    print(ans+1,index+1)
+      
+    
         
     
